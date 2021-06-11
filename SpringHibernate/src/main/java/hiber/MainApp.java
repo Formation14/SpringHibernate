4db4ecc3ec3f192bd344539efe64736984c759ab
Formation14 <paveltis@tut.by>
2021-06-11 16:00:38 +0300

    ver1

--- SpringHibernate/src/main/java/hiber/MainApp.java
+++ SpringHibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,6 +9,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.sql.SQLException;
 import java.util.List;
 
+/*
+Задание:
+1. Создайте соединение к своей базе данных и схему. Запустите приложение. Проверьте, что оно полностью работает.
+2. Создайте сущность Car с полями String model и int series, на которую будет ссылаться User с помощью связи one-to-one.
+3. Добавьте этот класс в настройки hibernate.
+4. Создайте несколько пользователей с машинами, добавьте их в базу данных, вытащите обратно.
+5. В сервис добавьте метод, который с помощью hql-запроса будет доставать юзера, владеющего машиной по ее модели и серии.
+ */
 public class MainApp {
    public static void main(String[] args) throws SQLException {
       AnnotationConfigApplicationContext context = 
@@ -15,10 +24,19 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      user1.setCar(new Car("Ford",150));
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      user2.setCar(new Car("Lada",5));
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      user3.setCar(new Car("BMW",3));
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+      user4.setCar(new Car("Mercedes",200));
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +44,12 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println(user.getCar());
          System.out.println();
       }
 
+      List <User> user = userService.getUser("Ford", 150);
+      System.out.println(user);
       context.close();
    }
 }
--- SpringHibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ SpringHibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,9 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,19 +13,29 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+    public List <User> getUser(String model, int series) {
+        Session session = sessionFactory.getCurrentSession();
+        String queryString = "SELECT u FROM User u INNER JOIN u.car c WHERE c.model = :model and c.series = :series";
+        Query queryObject = session.createQuery(queryString);
+        queryObject.setParameter("model", model);
+        queryObject.setParameter("series", series);
+        List<User> list = queryObject.list();
+        return list;
+    }
 }
